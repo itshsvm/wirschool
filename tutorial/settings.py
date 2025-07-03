2307db6f0ede9e45a19117fe42b5d52a18ccad6b
Hesam <ihesvm80@gmail.com>
2025-07-03 15:40:30 +0330

    new file

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database' / 'db.sqlite3',
     }
 }
 
